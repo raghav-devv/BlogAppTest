f3c67913d74d7ade81ee31e651da06214820df5b
Raghav <raghavkhajuria787@gmail.com>
2025-03-18 18:27:25 +0530

    Revert "                                                                                                                                  iqa"
    This reverts commit 5ce42a189214a114022d966d3e76317e3dc7b6fb.

--- Post/src/main/java/com/example/Post/Entity/Post.java
+++ Post/src/main/java/com/example/Post/Entity/Post.java
@@ -1,8 +1,11 @@
 
 package com.example.Post.Entity;
 
-import javax.persistence.*;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- comment/comment/src/main/java/com/example/Comment/Entity/Post.java
+++ comment/comment/src/main/java/com/example/Comment/Entity/Post.java
@@ -1,6 +1,10 @@
 package com.example.Comment.Entity;
 
-import javax.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
